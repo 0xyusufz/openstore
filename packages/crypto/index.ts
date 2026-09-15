11bf8638e6aa12b366243dd993fbc15728c0b3e3
0xyusufz <gurucoc471@gmail.com>
2026-09-15 13:04:11 +0530

    chore: initialize OpenStore project

---
+++ packages/crypto/index.ts
@@ -0,0 +1,14 @@
+/**
+ * OpenStore Cryptography Module
+ *
+ * Responsible for:
+ * - Encrypting storage chunks
+ * - Decrypting storage chunks
+ * - Generating encryption keys
+ * - Protecting chunk confidentiality and integrity
+ *
+ * MVP algorithm:
+ * AES-256-GCM
+ */
+
+export const CRYPTO_VERSION = 1;
